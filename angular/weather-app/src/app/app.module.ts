5f05e0d7ee2c6e94901ac6562e29d123c04942a4
byter <byter>
2021-09-24 17:16:05 -0400

    estudos novos

---
+++ angular/weather-app/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeatherComponent } from './weather/weather.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [AppComponent, WeatherComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+  ],
+
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ angular/weather-app/src/app/weather.service.ts
@@ -0,0 +1,23 @@
+import { environment } from './../environments/environment';
+import { Weather } from './weather';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  constructor(private http: HttpClient) {}
+
+  getWeather(city: string): Observable<Weather> {
+    const options = new HttpParams()
+      .set('units', 'metric')
+      .set('q', city)
+      .set('appId', environment.apiKey);
+
+    return this.http.get<Weather>(environment.apiUrl + 'weather', {
+      params: options,
+    });
+  }
+}
